27314aed64c936a85b8228e819ac3fc5be8ae7ee
Nanaloveyuki <miaomiao_159@outlook.com>
2026-06-21 21:54:07 +0800

    修复css404问题

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,13 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 
+const site = process.env.SITE_URL ?? 'https://naloveyuki.top';
+const siteUrl = new URL(site);
 const repository = process.env.GITHUB_REPOSITORY?.split('/')[1] ?? 'Nanaloveyuki';
 const owner = process.env.GITHUB_REPOSITORY_OWNER ?? 'Nanaloveyuki';
 const isUserSite = repository.endsWith('.github.io');
-const base = process.env.GITHUB_ACTIONS && !isUserSite ? `/${repository}` : '/';
-const site =
-  process.env.SITE_URL ?? `https://${owner}.github.io${isUserSite ? '' : `/${repository}`}`;
+const isDefaultGithubPagesHost = siteUrl.hostname === `${owner}.github.io`;
+const base = !isDefaultGithubPagesHost || isUserSite ? '/' : `/${repository}/`;
 
 // https://astro.build/config
 export default defineConfig({
